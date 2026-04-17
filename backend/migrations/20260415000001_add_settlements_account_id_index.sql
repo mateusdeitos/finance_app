e82de56bdb751699ecbfd270897c918782b7c8a1
Mateus Campos Deitos <mate.deitos@hotmail.com>
2026-04-16 22:26:39 -0300

    fix migration

--- backend/migrations/20260415000001_add_settlements_account_id_index.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE INDEX idx_settlements_account_id ON settlements(account_id);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-DROP INDEX IF EXISTS idx_settlements_account_id;
--- +goose StatementEnd
